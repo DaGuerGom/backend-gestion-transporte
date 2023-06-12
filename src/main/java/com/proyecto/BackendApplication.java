2c2340b15c2cef34927190225855635d06da3af6
David Guerrero Gómez <95078399+DaGuerGom@users.noreply.github.com>
2023-06-12 17:13:47 +0200

    Cambiado el CORS para permitir front desplegado

--- src/main/java/com/proyecto/BackendApplication.java
+++ src/main/java/com/proyecto/BackendApplication.java
@@ -20,7 +20,7 @@ public class BackendApplication {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200")
+                    .allowedOrigins("*")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                     .allowCredentials(true);
         }
